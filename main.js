42f6b79295e3ef53251e14e6938808642b9fa888
KIMHYESU <kimss9209@gmail.com>
2022-07-28 20:43:18 +0900

    ADD: map,forEach 메서드 사용 및 fetch 함수를 이용한 페이지 보여주기 완성

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let news = [];
+let menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByTopic(event))
+);
+
 const getLatestNews = async () => {
   let url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`
@@ -11,6 +16,48 @@ const getLatestNews = async () => {
   let data = await response.json();
   news = data.articles;
   console.log(news);
+  render();
+};
+
+const getNewsByTopic = async (event) => {
+  let topic = event.target.textContent.toLowerCase();
+  let url = new URL(
+    `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`
+  );
+  let header = new Headers({
+    "x-api-key": "kea8lyj_sWmvtQq7540wXqaR7QmGH3j7qTJ5PO5mrz8",
+  });
+
+  let response = await fetch(url, { headers: header });
+  let data = await response.json();
+  news = data.articles;
+  console.log(news);
+  render();
+};
+
+const render = () => {
+  let newsHTML = "";
+  newsHTML = news
+    .map((item) => {
+      return `<div class="row news">
+    <div class="col-lg-4">
+      <img
+        class="newsImgSize"
+        src="${item.media}"
+      />
+    </div>
+    <div class="col-lg-8">
+      <h2>${item.title}</h2>
+      <p>${item.summary}</p>
+      <div>${item.rights} * ${item.published_date}</div>
+    </div>
+  </div>
+`;
+    })
+    .join(" ");
+
+  console.log(newsHTML);
+  document.getElementById("newsBoard").innerHTML = newsHTML;
 };
 
 getLatestNews();
