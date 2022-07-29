95f2b6b3c95d66e1502d6017edd565337aa9c5cb
KIMHYESU <kimss9209@gmail.com>
2022-07-29 18:03:40 +0900

    ADD: 페이지네이션 기능 추가

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let news = [];
+let page = 1;
+let totalPages = 0;
 let menus = document.querySelectorAll(".menus button");
 menus.forEach((menu) =>
   menu.addEventListener("click", (event) => getNewsByTopic(event))
@@ -13,15 +15,23 @@ const getNews = async () => {
       "x-api-key": "kea8lyj_sWmvtQq7540wXqaR7QmGH3j7qTJ5PO5mrz8",
     });
 
+    url.searchParams.set("page", page);
+    console.log("url은??", url);
+
     let response = await fetch(url, { headers: header });
     let data = await response.json();
     if (response.status == 200) {
       if (data.total_hits == 0) {
         throw new Error("검색된 결과값이 없습니다.");
       }
+
       news = data.articles;
+      totalPages = data.totalPages;
+      page = data.page;
+
       console.log(news);
       render();
+      pagenation();
     } else {
       throw new Error(data.message);
     }
@@ -91,6 +101,60 @@ const errorRender = (message) => {
   ${message}
 </div>`;
   document.getElementById("newsBoard").innerHTML = errorHTML;
+};
+
+const pagenation = () => {
+  let pagenationHTML = ``;
+  // total page 수
+  // 내가 보고있는 page
+
+  // page group
+  let pageGroup = Math.ceil(page / 5);
+  // last page
+  let last = pageGroup * 5;
+  // first page
+  let first = last - 4;
+
+  // first~last page print
+  pagenationHTML = ` <li class="page-item" onClick="moveToPage(1)">
+  <a class="page-link" href="#" aria-label="Previous">
+    <span aria-hidden="true">&lt;&lt;</span>
+  </a>
+</li><li class="page-item">
+  <a class="page-link" href="#" aria-label="Previous" onClick="moveToPage(${
+    page - 1
+  })">
+    <span aria-hidden="true">&lt;</span>
+  </a>
+</li>`;
+
+  for (let i = first; i <= last; i++) {
+    pagenationHTML += `<li class="page-item ${
+      page == i ? "active" : ""
+    }"><a class="page-link" href="#" onClick="moveToPage(${i})">${i}</a></li>`;
+  }
+
+  pagenationHTML += ` <li class="page-item">
+  <a class="page-link" href="#" aria-label="Next" onClick="moveToPage(${
+    page + 1
+  })">
+    <span aria-hidden="true">&gt;</span>
+  </a>
+</li>     <li class="page-item" onClick="moveToPage(${last})">
+<a class="page-link" href="#" aria-label="Next">
+  <span aria-hidden="true">&gt;&gt;</span>
+</a>
+</li>`;
+
+  document.querySelector(".pagination").innerHTML = pagenationHTML;
+};
+
+const moveToPage = (pageNum) => {
+  //1. 이동하고싶은 페이지를 알아야한다.
+  page = pageNum;
+
+  //2. 이동하고싶은 페이지를 가지고 api를 다시 호출해야한다.
+  getNews();
 };
 
 searchButton.addEventListener("click", getNewsByKeyword);
