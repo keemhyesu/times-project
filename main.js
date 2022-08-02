107f59d0ad6289ca39dad37cfc69a4086df05acb
KIMHYESU <kimss9209@gmail.com>
2022-08-02 22:08:52 +0900

    ADD: HTML, CSS
    - 헤드라인 로고 삽입 및 사이즈 수정
    - grid 속성의 row,col 적용
    - display: flex 속성 적용
    - flex-wrap으로 상단 border 튀어나온거 다듬기 완료

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const getLatestNews = () => {
+let news = []; //articles 뽑아내기 위한 배열
+const getNews = async () => {
   let url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`
   );
   let header = new Headers({
     "x-api-key": "kea8lyj_sWmvtQq7540wXqaR7QmGH3j7qTJ5PO5mrz8",
   });
-  let response = fetch(url, { headers: header });
+  let response = await fetch(url, { headers: header });
+  let data = await response.json();
+  news = data.articles; // 우리가 보여주려는 데이터는 articles에 있음
+  console.log("뉴스", news);
 };
 
-getLatestNews();
+getNews();
